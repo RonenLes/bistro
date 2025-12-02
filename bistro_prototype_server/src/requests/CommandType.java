2aa8547fa03b202272a37414802734729dafd139
RoL <142698414+RonenLes@users.noreply.github.com>
2025-12-02 13:30:18 +0200

    Delete bistro_prototype_server directory
    useless copy

--- bistro_prototype_server/src/requests/CommandType.java
+++
@@ -1,9 +0,0 @@
-package requests;
-
-import java.io.Serializable;
-
-public enum CommandType implements Serializable {
-	
-	READ_ALL_EXISTING_RESERVATIONWS,//used by ShowDataRequest
-	READ_RESERVATIONS_BY_CODE;//used by ShowDataRequest
-}

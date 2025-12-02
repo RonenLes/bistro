0773ecf4dd1803127be7d5540bff590757d35e50
RoL <ronenles@gmail.com>
2025-12-02 14:30:16 +0200

    updated gitignore so it will open in eclipse

---
+++ Assignment 2/bistro_prototype/.metadata/.plugins/org.eclipse.core.resources/.history/3e/e057aef0b7ce001012d3b00a8bd2b002
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module bistro_prototype_client {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens application.ui to javafx.graphics, javafx.fxml;
+    opens controllers;
+    opens entities;
+    opens requests;
+    opens responses;
+}
--- Assignment 2/bistro_prototype/bistro_prototype_client/src/application/ClientMain.java
+++ Assignment 2/bistro_prototype/bistro_prototype_client/src/application/ClientMain.java
@@ -8,16 +8,16 @@ public class ClientMain {
 
         boolean connected = false;
 
-        // 1. Create the client (no controller yet)
+        // Create the client 
         BistroClient client = new BistroClient("localhost", 3000);
 
-        // 2. Create the controller with the client
+        // Create the controller with the client
         ClientController controller = new ClientController(client);
 
-        // 3. Inject controller back into the client
+        // Inject controller back into the client
         client.setClientController(controller);
 
-        // 4. Open connection
+        // Open connection
         try {
             client.openConnection();
             connected = true;
@@ -25,7 +25,7 @@ public class ClientMain {
             System.out.println("Connection failed: " + e.getMessage());
         }
 
-        // 5. Start JavaFX UI with the controller & connection status
+        // Start JavaFX UI with the controller & connection status
         ClientUI.startUI(controller, connected);
     }
 }

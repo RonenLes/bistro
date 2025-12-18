912893a41b159fb9bec24e0d257a45f0e736cc21
NirRochlin <nirrochlin23@gmail.com>
2025-12-18 18:30:18 +0200

    oof

--- Assignment 3/Bistro/bistro_Server/src/controllers/ReservationControl.java
+++ Assignment 3/Bistro/bistro_Server/src/controllers/ReservationControl.java
@@ -1,5 +1,32 @@
 package controllers;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+import database.ReservationDAO;
 
 public class ReservationControl {
+	private final ReservationDAO reservationDAO;
+	
+	public boolean isReservationDateTimeInAllowedRange(LocalDate reservationDate,LocalTime startTime) {
+		if (reservationDate == null || startTime == null) return false;
 
+
+		LocalDateTime arrivalLocal = LocalDateTime.of(reservationDate, startTime);
+
+
+		ZoneId zone = ZoneId.of("Asia/Jerusalem");
+		ZonedDateTime now = ZonedDateTime.now(zone);
+
+		ZonedDateTime arrival = arrivalLocal.atZone(zone);
+		
+		ZonedDateTime minAllowed = now.plusHours(1);     
+		ZonedDateTime maxAllowed = now.plusMonths(1);   
+
+
+		return !arrival.isBefore(minAllowed) && !arrival.isAfter(maxAllowed);
+	}	
+	
 }
